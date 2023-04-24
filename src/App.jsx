c4c957e6e41fd5b6da47dcb959a3add4d7ecb7cd
manaomimasha <manaomi.masha@gmail.com>
2023-04-24 10:40:52 -0300

    hjhj

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,30 @@ function App() {
 
   return (
     <>
-    <h1 >Masajes Masha</h1>
-{/* <nav className='contenedor'>
+      <h1 >Masajes Masha</h1>
+      {/* <nav className='contenedor'>
   <a>Reservar</a>
   <a>Informacion y tipos de masajes</a>
   <a>Precios y Ubicacion</a>
   <a>Tratamientos faciales</a>
   <a>Contacto</a>
 </nav> */}
-<div className='contenedor info '>
-<h> Reserva hoy tu masaje:</h>
-<p>
-<a href=" https://www.facebook.com/masajestarima/">Reservar mi turno por Faceebok citas</a>
-<p> Escribir Whatsap a +54 1130912600
-  
-</p>
+      <div className='contenedor info '>
+        <h> Reserva hoy tu masaje:</h>
+        <p>
+          <a href=" https://www.facebook.com/masajestarima/">Reservar mi turno por Faceebok citas</a>
+          <p> Escribir Whatsap a +54 1130912600
 
-</p>
-</div>
+
+          </p>
+
+          <p className='contenedor1'> Consultar disponibilidad de turnos:
+            Lunes a Sabados 10 a 16hs y 17.30 a 20hs.
+            Ubicacion: Av. San Martin 2465, Florida, Vicente Lopez.
+          </p>
+
+        </p>
+      </div>
 
 
 
